39474306437b81aff7e1304f28862ec9112b026e
ELvan Surahmat <146643689+ElvanSurahmat@users.noreply.github.com>
2025-06-21 05:41:42 +0700

    public set up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">
+      <h1 className="text-5xl text-violet-300 font-circular-web">
         Welcome to the React App!
       </h1>
     </main>
